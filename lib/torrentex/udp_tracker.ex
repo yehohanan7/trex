14d63bcd9c7c54ca058c68cdd6f69754808f4ec2
Johnvp <johnvp@john.corporate.thoughtworks.com>
2014-05-20 18:33:38 +0530

    upd tracker, connect request

--- lib/torrentex/udp_tracker.ex
+++ lib/torrentex/udp_tracker.ex
@@ -2,6 +2,11 @@ defmodule Torrentex.UDPTracker do
   use GenServer.Behaviour
   alias Torrentex.Url
 
+  @actions %{:connect   => 0,
+             :announce  => 1,
+             :scrape    => 2,
+             :error     => 3}
+
   #External API
   def start_link(port) do
     :gen_server.start_link({:local, :tracker}, __MODULE__, port, [])
@@ -18,9 +23,11 @@ defmodule Torrentex.UDPTracker do
   end
 
   def handle_cast({:track, torrent}, state) do
-    IO.inspect "sending announce request to #{torrent[:announce]}"
-    send_announce_request(state[:socket], torrent)
-    IO.inspect "announce request sent!"
+    IO.inspect "sending connect request to #{torrent[:announce]}"
+    send_message({state[:socket], torrent}, :connect)
+    # IO.inspect "sending announce request to #{torrent[:announce]}"
+    # send_message({state[:socket], torrent}, :announce)
+    # IO.inspect "announce request sent!"
     {:noreply, state}
   end
 
@@ -28,14 +35,28 @@ defmodule Torrentex.UDPTracker do
     {:reply, "status", state}
   end
 
-  def handle_info({:udp, _, _ip, _port, packet}, state) do
-    IO.inspect "Data recieved on udp socket.."
-    {:noreply, state}
+  #Utilities
+  defp generate_transaction_id do
+    :crypto.rand_bytes(4)
   end
 
-  def send_announce_request(socket, torrent) do
+  #Outgoing messages
+  defp send_message({socket, torrent}, :announce) do
     [domain, port] = Url.host(torrent[:announce])
     :ok = :gen_udp.send(socket, domain, port, "data")
+  end
+
+  defp send_message({socket, torrent}, :connect) do
+    [domain, port] = Url.host(torrent[:announce])
+    :ok = :gen_udp.send(socket, domain, port, <<4497486125440::64,@actions[:connect]::32, generate_transaction_id::binary>>)
+  end
+
+
+  #Incoming messages from socket
+  def handle_info({:udp, _, _ip, _port, packet}, state) do
+    IO.inspect "Data recieved on udp socket.."
+    IO.inspect packet
+    {:noreply, state}
   end
 
 end
