4f0c69c0c8243d3181ff619e0e16b9f4fe36faa5
Vivek <vivektheone@gmail.com>
2014-06-09 08:02:37 +0100

    Initial UI

--- gui/trex.js
+++ gui/trex.js
@@ -1,5 +1,5 @@
 chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('trex.html', {
+  chrome.app.window.create('index.html', {
     'bounds': {
       'width': 400,
       'height': 500
