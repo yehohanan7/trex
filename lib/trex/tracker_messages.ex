c5c6035ce062cfaa486952e5303bab34ffce91d7
Johnvp <johnvp@john.corporate.thoughtworks.com>
2014-06-11 15:57:19 +0530

    refactorings

--- lib/trex/tracker_messages.ex
+++ lib/trex/tracker_messages.ex
@@ -11,20 +11,48 @@ defmodule Trex.Tracker.Messages do
             :started   => 2,
             :stopped   => 3}
 
+  defp to_binary({value, :full}) when is_binary(value) do
+    <<value::binary>>
+  end
+
+  defp to_binary({value, bytes}) when is_binary(value) do
+    <<value::[size(bytes), binary]>>
+  end
+
+  defp to_binary({value, bytes}) do
+    bits = bytes * 8
+    <<value::[size(bits)]>>
+  end
+  
+  defp to_binary(data) when is_list(data) do
+    Enum.reduce(Keyword.values(data), <<>>, fn (x, acc) -> acc <> to_binary(x) end)
+  end
 
   def connect_request(transaction_id) do
-    <<4497486125440::64,@actions[:connect]::32, transaction_id::binary>>
+    [prefix:         {4497486125440, 8},
+     action:         {@actions[:connect], 4},
+     transaction_id: {transaction_id, :full}]
+    |> to_binary
   end
 
   def announce_request(transaction_id, connection_id, torrent) do
-    peer_id = 1; ip = key = uploaded = downloaded = left = 0; num_want = 50; port = 9998
-    <<connection_id::[size(8), binary], @actions[:announce]::32, transaction_id::[size(4), binary], torrent[:info_hash]::[size(20), binary], 
-    peer_id::160,downloaded::64, left::64, uploaded::64, @events[:started]::32, ip::32, key::32, num_want::32, 9998::16>>
+    [connection_id:   {connection_id, 8},
+     action:          {@actions[:announce], 4},
+     transaction_id:  {transaction_id, 4},
+     info_hash:       {torrent[:info_hash], 20},
+     peer_id:         {1, 20},
+     downloaded:      {0, 8},
+     left:            {0, 8},
+     uploaded:        {0, 8},
+     event:           {@events[:started], 4},
+     ip:              {0, 4},
+     key:             {0, 4},
+     num_want:        {50, 4},
+     port:            {9998, 2}]
+    |> to_binary       
   end
   
   def parse_response(packet, transaction_id) do
-    
-    IO.inspect packet
 
     case packet do
       <<0::32, transaction_id::[size(4), binary], connection_id::binary>> -> 
@@ -32,8 +60,9 @@ defmodule Trex.Tracker.Messages do
 
       <<1::32, transaction_id::[size(4), binary], interval::32, leechers::32, seeder::32, rest::binary>> ->
         decode_peer(rest)
+
       _ ->
-        :error
+        :unknown_response
     end
 
   end
