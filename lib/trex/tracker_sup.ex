08d63c1d74fa1659083a4dc09e91a9320cb374a2
Johnvp <johnvp@john.corporate.thoughtworks.com>
2014-06-14 22:49:10 +0530

    refactored tracker_sup

--- lib/trex/tracker_sup.ex
+++ lib/trex/tracker_sup.ex
@@ -11,17 +11,19 @@ defmodule Trex.TrackerSupervisor do
     supervise([], strategy: :one_for_one)
   end
 
+  def start_tracker(port, url, module, torrent) do
+    id = string_to_atom("#{url}_#{torrent[:id]}")
+    :supervisor.start_child(:tracker_sup, worker(module, [id, port, url, torrent], [id: id]))
+  end
+
   def start_tracker(<<"http", _::binary>> = url, port, torrent) do
     IO.inspect "starting tcp tracker..."
-    id = string_to_atom("#{url}_#{torrent[:id]}")
-    :supervisor.start_child(:tracker_sup, worker(Trex.TCPTracker, [id, port, url, torrent], [id: id]))
+    start_tracker(port, url, Trex.TCPTracker, torrent)
   end
 
   def start_tracker(<<"udp", _::binary>> = url, port, torrent) do
     IO.inspect "starting udp tracker..."
-    id = string_to_atom("#{url}_#{torrent[:id]}")
-    :supervisor.start_child(:tracker_sup, worker(Trex.UDPTracker, [id, port, url, torrent], [id: id]))
+    start_tracker(port, url, Trex.UDPTracker, torrent)
   end
-
 
 end
