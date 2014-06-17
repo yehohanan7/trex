619d0ddf5a68ec369456e8977c37844620f77c7c
Johnvp <johnvp@john.corporate.thoughtworks.com>
2014-06-17 15:55:04 +0530

    module attribute corrected

--- lib/trex/udp_tracker.ex
+++ lib/trex/udp_tracker.ex
@@ -5,7 +5,7 @@ defmodule Trex.UDPTracker do
   alias Trex.Peer
   import Trex.Tracker.Messages
 
-  @TIME_OUT @TIME_OUT
+  @time_out 0
 
   #External API
   def start_link(id, port, url, torrent) do
@@ -15,7 +15,7 @@ defmodule Trex.UDPTracker do
 
   #GenFSM Callbacks
   def init({port, tracker_host, tracker_port, torrent}) do
-    {:ok, :initialized, %{remote_tracker: {tracker_host, tracker_port}, torrent: torrent, port: port}, @TIME_OUT}
+    {:ok, :initialized, %{remote_tracker: {tracker_host, tracker_port}, torrent: torrent, port: port}, @time_out}
   end
 
   def terminate(_reason, _statename, _state) do
@@ -24,7 +24,7 @@ defmodule Trex.UDPTracker do
 
   #States
   def initialized(event, %{port: port} = state) do
-    {:next_state, :connector_ready, Dict.put(state, :connector, Connector.new(port, self())), @TIME_OUT}
+    {:next_state, :connector_ready, Dict.put(state, :connector, Connector.new(port, self())), @time_out}
   end
   
   def connector_ready(_event, state) do
@@ -35,7 +35,7 @@ defmodule Trex.UDPTracker do
   
   def awaiting_connection(packet, state) do
     {:connection_id, connection_id} = parse_response(packet, state[:transaction_id])
-    {:next_state, :connected, Dict.put(state, :connection_id, connection_id), @TIME_OUT}
+    {:next_state, :connected, Dict.put(state, :connection_id, connection_id), @time_out}
   end
 
   def connected(_event, %{torrent: torrent, transaction_id: transaction_id, connection_id: connection_id} = state) do
@@ -46,10 +46,10 @@ defmodule Trex.UDPTracker do
   
   def announcing(packet, state) do
     IO.inspect "announce response received"
-    {:next_state, :peers_determined, state, @TIME_OUT}
+    {:next_state, :peers_determined, state, @time_out}
   end
 
-  def peers_determined(_event, %{torrent: torrent} = state) do
+  def peers_determined(_, %{torrent: torrent} = state) do
     IO.inspect "remote peers publishing to local peer...."
     Peer.peers_found(torrent[:id], [:peer1, :peer2])
     {:next_state, :peers_determined, state}
