e71e4db0912c4df5004eb5e7232c55d92efe4ba5
Johnvp <johnvp@john.corporate.thoughtworks.com>
2014-08-21 02:48:22 +0530

    peer handshake implemented

--- lib/trex/peer.ex
+++ lib/trex/peer.ex
@@ -5,6 +5,10 @@ defmodule Trex.Peer do
 
   @time_out 0
 
+  @peer_id 1234
+  @handshake_header <<19::8, "BitTorrent protocol", 0::64>>
+  @handshake_response_size 68
+
   #External API
   def start({host, port}, tpid) do
     PeerSupervisor.start_peer(host, port, tpid)
@@ -16,20 +20,37 @@ defmodule Trex.Peer do
 
   #GenServer Callbacks
   def init({host, port, tpid}) do
-    {:ok, :initialized, %{host: host, port: port, tpid: tpid}, @time_out}
+    {:ok, :initializing, %{host: host, port: port, tpid: tpid}, @time_out}
   end
 
   def terminate(_reason, _statename, _state) do
     :ok
   end
 
-  def initialized(_event, %{host: host, port: port} = state) do
-    IO.inspect "peer initialized for #{host} #{port}"
-    {:next_state, :initialized, state}
+  def initializing(:timeout, %{host: host, port: port, tpid: tpid} = state) do
+    case :gen_tcp.connect(to_char_list(host), port, [:binary, {:active, false}]) do
+      {:ok, sock} -> {:next_state, :initialized, %{sock: sock, tpid: tpid}, @time_out}
+      {:error, reason} -> {:stop, "error while connecting to peer... #{self}", state}
+    end
   end
 
-  def handle_info(_timeout, state) do
-    {:noreply, state}
+
+  def initialized(_event, %{sock: sock, tpid: tpid} = state) do
+    :ok = :gen_tcp.send(sock, @handshake_header)
+    :ok = :gen_tcp.send(sock, Torrent.infohash(tpid) |> Hex.decode)
+    :ok = :gen_tcp.send(sock, <<@peer_id::160>>)
+    {:next_state, :handshake_initiated, state, @time_out}
+  end
+
+  def handshake_initiated(event, %{sock: sock} = state) do
+    case :gen_tcp.recv(sock, @handshake_response_size) do
+      {:ok, data} -> IO.inspect "hand shake response recieved.."; IO.inspect data; {:next_state, :handshake_completed, state}
+      {:error, reason} -> {:stop, "error while handshaking..", state}                                                                            
+    end
+  end
+
+  def handle_info(msg, statename, state) do
+    {:stop, "unknown message... #{statename}", state}
   end
 
 end
